52af9605631bd9e658b4d54add7cb820186c61cf
jlisita <lisita_julien@hotmail.fr>
2024-09-20 04:26:57 +0200

    Fetch data from API for categories, topics, comments, and users in ForumTopics component

--- src/components/public/ForumTopics.jsx
+++ src/components/public/ForumTopics.jsx
@@ -1,24 +1,74 @@
 import "./ForumTopics.css"
 import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import users from './../../assets/data/users.json';
-import categories from './../../assets/data/categories.json';
-import topics from './../../assets/data/topics.json';
-import comments from './../../assets/data/comments.json';
+import { useState, useEffect } from "react";
+
 
 const ForumTopics = () => {
 
     const { category_id } = useParams();
 
+    const [categories, setCategories] = useState([]);
+    const [topics, setTopics] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/categories')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setCategories(data.data)
+                console.log(`categorie = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des catégories:', error));
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/topics')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setTopics(data.data)
+                console.log(`topics = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des topics:', error));
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/comments')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setComments(data.data)
+                console.log(`comments = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des commentaires:', error));
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setUsers(data.data)
+                console.log(`users = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des utilisateurs:', error));
+    }, []);
 
 
     const getCategoryById = (category_id) => categories.find(category => category.id == category_id);  
 
-    const topicsByCategory = (category_id) => topics.filter((topic) => topic.category_id == category_id);
+    const topicsByCategory = (category_id) => topics.filter((topic) => topic.CategoryId == category_id);
 
     const sortedTopicsByCategory = (category_id) => topicsByCategory(category_id).sort((a,b) => new Date(b.created_at) - new Date(a.created_at)); 
 
-    const commentsByTopic = (topic_id) => comments.filter(comment => comment.topic_id == topic_id);
+    const commentsByTopic = (topic_id) => comments.filter(comment => comment.TopicId == topic_id);
     
     const lastCommentByTopic = (topic_id) => {
             return commentsByTopic(topic_id).sort((a,b) => new Date(b.created_at) - new Date(a.created_at))[0];
@@ -26,19 +76,22 @@ const ForumTopics = () => {
     
     const getUserById = (user_id) => users.find(user => user.id == user_id);
 
-    const lastCommentByTopicUser = (topic_id) => getUserById( lastCommentByTopic(topic_id) && lastCommentByTopic(topic_id).user_id);
+    const lastCommentByTopicUser = (topic_id) => getUserById( lastCommentByTopic(topic_id) && lastCommentByTopic(topic_id).UserId);
 
     const handleNewTopic = (e) => 
         {
             setSortGameOption(e.target.value);
         };
 
+    const category = getCategoryById(category_id);
+    const categoryTitle = category ? category.title : "Catégorie non trouvée";
+
     return (
         <div>
             <section className="bannerTopics banner">
             </section>
             <section className="topics main-content">
-                <h1 className="main-content-title">Catégorie: {getCategoryById(category_id).title}</h1>
+                <h1 className="main-content-title">Catégorie: {categoryTitle}</h1>
                 <h2>Liste des topics</h2>
                 <div className="topics-list">
                     <table className="topics-list-table">
