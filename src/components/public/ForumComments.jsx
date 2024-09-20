e1cd51db08f6a8c8e5f903ec3fe6b5503d4a15cc
jlisita <lisita_julien@hotmail.fr>
2024-09-20 04:28:14 +0200

    Fetch data from API for topics, comments, and users in ForumComment component

--- src/components/public/ForumComments.jsx
+++ src/components/public/ForumComments.jsx
@@ -1,21 +1,59 @@
 import "./ForumComments.css"
 import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import users from './../../assets/data/users.json';
-import categories from './../../assets/data/categories.json';
-import topics from './../../assets/data/topics.json';
-import comments from './../../assets/data/comments.json';
+import { useState, useEffect } from "react";
 
 const ForumComments = () => {
     
 
     const { topic_id } = useParams();
+
+    const [topics, setTopics] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/topics')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setTopics(data.data)
+                console.log(`topics = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des topics:', error));
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/comments')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setComments(data.data)
+                console.log(`comments = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des commentaires:', error));
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/users')
+        .then((response) => {
+            return response.json();
+            })
+            .then((data) => {
+                setUsers(data.data)
+                console.log(`users = ${data.data}`);
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des utilisateurs:', error));
+    }, []);
+
     
     const getTopicById = (topic_id) => topics.find(topic => topic.id == topic_id);
 
     const getUserById = (user_id) => users.find(user => user.id == user_id);
 
-    const commentsByTopic = (topic_id) => comments.filter(comment => comment.topic_id == topic_id);
+    const commentsByTopic = (topic_id) => comments.filter(comment => comment.TopicId == topic_id);
 
     const sortCommentsByTopic = (topic_id) => commentsByTopic(topic_id).sort((a,b) => new Date(b.created_at) - new Date(a.created_at)); 
 
@@ -26,6 +64,7 @@ const ForumComments = () => {
     }
 
     const topic = getTopicById(topic_id);
+    const topicTitle = topic ? topic.title : "Topic non trouvé";
 
     
     return (
@@ -33,7 +72,7 @@ const ForumComments = () => {
             <section className="bannerComments banner">
             </section>
             <section className="comments main-content">
-                <h1 className="main-content-title">Topic: {topic.title}</h1>
+                <h1 className="main-content-title">Topic: {topicTitle}</h1>
                 <h2>Liste des commentaires</h2>
                 <div className="comments-list">
                     <table className="comments-list-table">
@@ -47,7 +86,7 @@ const ForumComments = () => {
                         <tbody>
                             {sortCommentsByTopic(topic_id).map((comment) => (
                             <tr key={comment.id}>
-                                <td><Link className="comments-list-table-username" to={`/users/${comment.user_id}`}>{getUserById(comment.user_id).username}</Link></td>
+                                <td><Link className="comments-list-table-username" to={`/users/${comment.UserId}`}>{getUserById(comment.UserId).username}</Link></td>
                                 <td>{comment.content}</td>
                                 <td>{`le ${new Date(comment.created_at).toLocaleDateString('fr-FR')} à ${new Date(comment.created_at).toLocaleTimeString()}`} </td>
                             </tr>
