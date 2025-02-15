268f4ce9921124b7e5641321736d5ddb43f3b9fa
Julien Lisita <julien.lisita@gmail.com>
2025-02-15 16:03:06 +0100

    create ChangePasswordForm component

---
+++ src/components/user/ChangePasswordForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useAuthContext } from "../../contexts/AuthContext.jsx";
+import ModalMessage from "../common/ModalMessage";
+
+const ChangePasswordForm = () => {
+
+    const {changePassword } = useAuthContext();
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [showMessageModal, setShowMessageModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState("");
+
+     // fonction de gestion du changement de mot de passe 
+     const handleChangePassword = async (e) => {
+        e.preventDefault();
+        
+        if (newPassword !== confirmPassword) {
+            openModal("Les nouveaux mots de passe ne correspondent pas.");
+            return;
+        }
+    
+        try {
+            const message = await changePassword(oldPassword, newPassword);
+            openModal(message); 
+            setOldPassword("");
+            setNewPassword("");
+            setConfirmPassword("");
+        } catch (error) {
+            openModal(error.message);
+        }
+    };
+  
+    return (
+
+        <form className="editProfil-content-passwordForm" onSubmit={handleChangePassword}>
+            <h2>Mot de passe</h2>
+            <div>
+                <label htmlFor="oldpassword">Ancien mot de passe</label><br />
+                <input name="oldpassword" id="oldpassword" type="password" value={oldPassword} onChange={(e)=>setOldPassword(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="newpassword">Nouveau mot de passe</label><br />
+                <input name="newpassword" id="newpassword" type="password" value={newPassword} onChange={(e)=>setNewPassword(e.target.value)}/>
+            </div>
+            <div>
+                <label htmlFor="passwordconfirmed">Confirmation du mot de passe</label><br />
+                <input name="passwordconfirmed" id="passwordconfirmed" type="password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
+            </div>
+            <div className="validationButton-container">
+                <button className="validationButton">Valider</button>
+            </div>
+            <ModalMessage 
+            isOpen={showMessageModal} 
+            message={modalMessage} 
+            onClose={()=>setShowMessageModal(false)} />  
+        </form>
+                    
+    )
+};
+
+export default ChangePasswordForm;
