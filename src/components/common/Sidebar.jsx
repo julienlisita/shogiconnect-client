8cce41382115cf9dfe5c735c7040ad025ed4a75c
Julien Lisita <julien.lisita@gmail.com>
2025-02-15 17:00:31 +0100

    display username in Sidebar component

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,19 +1,14 @@
 import image from "./../../assets/images/user.png"
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useAuthContext } from "../../contexts/AuthContext";
 import "./Sidebar.css"
 
 const Sidebar = ({ menuItems }) => 
 {
-    const user = {
-    id: 1,
-    username: "ShadowNinja",
-    country: "France",
-    isOnline: true,
-    biography: "Je suis un maître de la furtivité, et j'ai toujours un coup d'avance sur mes adversaires. Le Shogi est pour moi une manière de développer mon esprit tactique et de rester en éveil. Depuis des années, j'étudie les stratégies et les mouvements des grands maîtres du Shogi. J'aime aussi analyser chaque partie que je joue, en cherchant constamment à m'améliorer. Pour moi, le Shogi n'est pas seulement un jeu, c'est un art qui reflète l'harmonie entre la patience, la précision et la détermination. Mon objectif est de devenir un joueur reconnu sur la scène internationale.",
-    user_role_id: 1,
-    created_at: "2024-09-01 09:00:00"
-    }
+    const { user, isAuthenticated } = useAuthContext();
+    
+    const username = user.username;
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -32,7 +27,7 @@ const Sidebar = ({ menuItems }) =>
                 </div>
                 {/* Sidebar qui change de classe en fonction de son état */}
                 <div className={`sidebar ${isOpen ? 'open' : 'sidebar-hidden'}`}>
-                    <p className="sidebar-welcome">{`Bienvenue ${user.username}`}</p>
+                    <p className="sidebar-welcome">{`Bienvenue ${username}`}</p>
                     <div className="sidebar-avatar">
                         <img src={image} alt="" />
                     </div>
