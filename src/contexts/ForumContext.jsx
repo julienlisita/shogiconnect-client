fcf639c68f193916ed33aa58ab68112fa4641cbb
Julien Lisita <julien.lisita@gmail.com>
2025-02-20 00:01:06 +0100

    feat: update forumContext to integrate topic and comment hooks

--- src/contexts/ForumContext.jsx
+++ src/contexts/ForumContext.jsx
@@ -7,14 +7,14 @@ const ForumContext = createContext();
 
 export const ForumProvider = ({ children }) => {
     const { categories, loading: categoriesLoading, error: categoriesError } = useCategories();
-    const { topics, loading: topicsLoading, error: topicsError, createTopic } = useTopics();
-    const { comments, loading: commentsLoading, error: commentsError, createComment } = useComments();
+    const { topics, loading: topicsLoading, error: topicsError, createTopic, deleteTopic } = useTopics();
+    const { comments, loading: commentsLoading, error: commentsError, createComment, deleteComment } = useComments();
 
     const loading = categoriesLoading || topicsLoading || commentsLoading;
     const error = categoriesError || topicsError || commentsError;
 
     return (
-        <ForumContext.Provider value={{ categories, topics, comments, loading, error, createTopic, createComment}}>
+        <ForumContext.Provider value={{ categories, topics, comments, loading, error, createTopic, deleteTopic, createComment, deleteComment}}>
             {children}
         </ForumContext.Provider>
     );
