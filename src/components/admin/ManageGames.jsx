c8a1dd3ffc67363a7310c010d9ad06736242bb89
Julien Lisita <julien.lisita@gmail.com>
2025-02-20 22:38:03 +0100

    feat: add game deletion handling in ManageGames.jsx

--- src/components/admin/ManageGames.jsx
+++ src/components/admin/ManageGames.jsx
@@ -1,43 +1,36 @@
 import "./ManageGames.css";
-import games from './../../assets/data/games.json';
-import users from './../../assets/data/users.json';
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useUserContext } from "../../contexts/UserContext";
+import { useScheduledGameContext } from "../../contexts/ScheduledGameContext.jsx";
 
 const ManageGames = () => {
 
+    const { users, usersLoading, usersError} = useUserContext();
+    const { scheduledGames, scheduledGamesLoading, scheduledGamesError, deleteScheduledGame } = useScheduledGameContext();
     const [sortGameOption, setSortGameOption] = useState('organizer');
-
     const [showModal, setShowModal] = useState(false);
+    const [selectedGameId, setSelectedGameId] = useState(null);
 
-    const getUserById = (user_id) => users.find(user => user.id == user_id);
-    
-    const sortedGames = games.sort((a, b) => {
-        if (sortGameOption === 'date') {
-            return new Date(a.created_at) - new Date(b.created_at); 
-        } else if (sortGameOption === 'rendezVous') {
-            return new Date(a.rendez_vous_at) - new Date(b.rendez_vous_at); 
-        } else if(sortGameOption === 'organizer')
-        {
-            return getUserById(a.organizer_id).username.localeCompare(getUserById(b.organizer_id).username); 
-        }
-        return 0;
-    });
+    if (usersLoading || scheduledGamesLoading) return <p>Loading...</p>;
+    if (usersError) return <p>Error loading users: {usersError}</p>;
+    if (scheduledGamesError) return <p>Error loading forum: {forumError}</p>;
+
+    // Fonctions utilitaires
       
     const handleSortGameChange = (e) => 
     {
         setSortGameOption(e.target.value);
     };
 
-    const handleModifyGame = (e) =>
+    const handleDeleteGame = (gameId) =>
     {
-    }
-    const handleDeleteGame = (e) =>
-    {
+        setSelectedGameId(gameId)
         setShowModal(true); 
     }
-    const confirmDeletion = () => {
-        console.log('partie supprimé');
+    const confirmDeletion = () => 
+    {
+        deleteScheduledGame(selectedGameId)
         setShowModal(false);
     };
     
@@ -45,23 +38,39 @@ const ManageGames = () => {
         setShowModal(false); 
     };
 
-    const handleCreateGame = (e) =>
-    {
-    };
+    const getUserById = (user_id) => users.find(user => user.id == user_id);
+    
+    const sortedGames = scheduledGames.length > 0 ? scheduledGames.sort((a, b) => {
+        if(sortGameOption === 'rendezVous')
+        {
+            return new Date(a.rendezVousAt) - new Date(b.rendezVousAt); 
+        }
+        else if (sortGameOption === 'participant') {
+            const userA = getUserById(a.ParticipantId);
+            const userB = getUserById(b.ParticipantId);
+            return (userA?.username || "").localeCompare(userB?.username || ""); 
+        } else if(sortGameOption === 'organizer')
+        {
+            const userA = getUserById(a.OrganizerId);
+            const userB = getUserById(b.OrganizerId);
+            return (userA?.username || "").localeCompare(userB?.username || ""); 
+        }
+        return 0;
+    }): [];
 
     return (
         <div className="manageGames-container">
             <div className="manageGames">
-                <h1>Gestion des utilisateurs</h1>
+                <h1>Gestion des rendez-vous de partie</h1>
                 {/* Modal pour la confirmation de suppression d'une partie' */}
                 {showModal && (
                 <div className="modal-overlay">
                     <div className="modal">
                         <h2>Confirmation</h2>
                         <div className="modal-content">
-                            <p>Êtes-vous sûr de vouloir supprimer cette partie ? Cette action est irréversible.</p>
+                            <p>Êtes-vous sûr de vouloir supprimer cette partie ?</p>
                             <div className="validationButton-container">
-                                <button className="validationButton" style={{ backgroundColor: 'red'}}onClick={confirmDeletion}>Oui, supprimer</button>
+                                <button className="validationButton" style={{ backgroundColor: 'red'}}onClick={confirmDeletion}>Oui</button>
                             </div>
                             <div className="validationButton-container">
                                 <button className="validationButton" onClick={cancelDeletion}>Annuler</button>            
@@ -72,40 +81,59 @@ const ManageGames = () => {
                 )}
                 <form className="manageGames-displaySelection">
                     <select className = "orderSelect" id="sortBy" value={sortGameOption} name="sortBy" onChange={handleSortGameChange}>
-                        <option value="organizer">Par organisateur</option>
-                        <option value="date">Par date de création</option>
                         <option value="rendezVous">Par date de rendez-vous</option>
+                        <option value="organizer">Par organisateur</option>
+                        <option value="participant">Par participant</option>
                     </select>
                 </form>
                 <div className="manageGames-list">
                     <table className="manageGames-list-table">
                         <thead>
                             <tr>
-                                <th>Organisateur</th>
-                                <th>Date de création</th>
                                 <th>Date de la partie</th>
-                                <th>
-                                </th>
+                                <th>Organisateur</th>
+                                <th>Participant</th>
+                                <th></th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            {sortedGames.map((game) => (
-                            <tr key={game.id}>
-                                <td> <Link className = "manageUsers-list-table-username" to={`/users/${game.organizer_id}`}>{getUserById(game.organizer_id).username}</Link></td>
-                                <td>{`${new Date(game.created_at).toLocaleDateString('fr-FR')}`}</td>
-                                <td>{`${new Date(game.rendez_vous_at).toLocaleDateString('fr-FR')}`}</td>
-                                <td>    
-                                    <div className="manageUsers-list-table-buttonContainer">
-                                        <button className="manageGames-table-modifyButton button" onClick={handleModifyGame}>Modifier</button>
-                                        <button className="manageGames-table-deleteButton button" onClick={handleDeleteGame}>supprimer</button>
-                                    </div>            
-                                </td>
-                            </tr>
-                            ))}
+                        {sortedGames.length === 0 ? (
+                                <tr><td colSpan="4">Parties non disponibles</td></tr>
+                            ) : (
+                                sortedGames.map((game) => {
+                                    const organizer = getUserById(game.OrganizerId);
+                                    const participant = getUserById(game.ParticipantId);
+                                    return (
+                                        <tr key={game.id}>
+                                                                                     <td>{`Le ${new Date(game.rendezVousAt).toLocaleDateString('fr-FR')} ${new Date(game.rendezVousAt).toLocaleTimeString()}`}</td>
+                                            <td>
+                                                {organizer ? (
+                                                    <Link to={`/users/${organizer.id}`}>{organizer.username}</Link>
+                                                ) : (
+                                                    "Inconnu"
+                                                )}
+                                            </td>
+                                            <td>
+                                                {participant ? (
+                                                    <Link to={`/users/${participant.id}`}>{participant.username}</Link>
+                                                ) : (
+                                                    "Aucun"
+                                                )}
+                                            </td>
+                                            <td>
+                                                <button>Désinscrire</button>
+                                            </td>
+                                            <td>
+                                                <button onClick={() => handleDeleteGame(game.id)}>Supprimer</button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })
+                            )}
                         </tbody>
                     </table>
                 </div>
-                <button className="manageGames-CreateButton button" onClick={handleCreateGame}>Ajouter une partie</button>
             </div>   
         </div>
     )
