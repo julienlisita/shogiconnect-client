3d7deac62bbbf33413185f02f550b77eefbd28a2
Julien Lisita <julien.lisita@gmail.com>
2025-05-20 15:55:31 +0200

    feat: update club data source and redesign club cards

---
+++ src/assets/data/clubsData.js
@@ -0,0 +1,104 @@
+export const clubs = [
+        {
+            id: 1,
+            title: "Association de Jeunesse Franco-Japonaise Nantes",
+            adress: [
+                ["75 rue d’Allonville","44000 Nantes"]
+            ],
+            horaire: [
+                "Le mercredi soir de 20h à 23h, à l’Association Jeunesse France-Japon."
+            ], 
+            contact: ["nantes.shogi.ajfj@gmail.com"],
+            website: "https://ajfj.eu/",
+        },
+        {
+            id: 2,
+            title: "Association Shogi Alsace",
+            adress: [
+                ["Maison des jeux 38 Rte de Schirmeck","67200 Strasbourg"],
+                ["Boutique Konjaku 4 rue Vauban","68000 Kolmar"]
+            ],
+            horaire: [
+                "Le Lundi soir de 20:00 à 23:00 sur Strasbourg, à la Maison des Jeux – 38 Rte de Schirmeck",
+                "Le Mercredi soir à partir de 20:00 sur Colmar, 4 rue Vauban",
+                "Ponctuellement sur Belfort, à l’institution Ste Marie, 40 faubourg des Ancêtres."
+            ], 
+            contact: [" alsace@shogi.fr", "+33 677849449"],
+            website: "https://shogialsace.wordpress.com/",
+        },
+        {
+            id: 3,
+            title: "Association Shogi Paris",
+            adress: [
+                ["94 rue d’Estienne d’Orves","92500 Rueil Malmaison"]
+            ],
+            horaire: [
+                "Les mardis soir de 19:00 à 23:00, au 94 rue d’Estienne d’Orves , Rueil Malmaison"
+            ], 
+            contact: ["https://ligue-idf-shogi.fr", "https://discord.gg/xJYGBht", "+33 6 67725576"],
+            website: "https://ajfj.eu/",
+        },
+        {
+            id: 4,
+            title: "Club d’échecs et de Shogi de Saint-Menoux",
+            adress: [["Rue des Essanyiers","03210 Saint-Menoux"]],
+            horaire: [
+                "Les jeudis de 16h30 à 18h (licences B et jeu libre pour les licences A)",
+                "Les samedis de 10h00 à 12h (licences A uniquement)"
+            ], 
+            contact: ["cyrilmeyzonnier@icloud.com", "+33 4 70 43 90 34"],
+            website: "https://www.facebook.com/clubdechecsetdeshogidesaintmenoux",
+        },
+        {
+            id: 5,
+            title: "Club de Shogi Bisontin",
+            adress: [["27 Quai de Strasbourg","25000 Besançon"]],
+            horaire: [
+                "Le Lundi de 18h30 à 20h30"
+            ], 
+            contact: ["shogi.besancon@gmail.com"],
+            website: "https://sites.google.com/site/clubdeshogibisontin",
+        },
+        {
+            id: 6,
+            title: "Club de Shogi de Grenobles",
+            adress: [["43 Avenue Alsace Lorraine","38000 Grenoble"]],
+            horaire: [
+                "Le Lundi soir à partir de 19h au bar la Nat’, 34 Bd Gambetta à Grenoble",
+                "Le Mercredi soir à partir de 19:00 au restaurant le Pav-San, 43 Av. Alsace Lorraine à Grenobles"
+
+            ], 
+            contact: ["shogi.grenoble@gmail.com"],
+            website: "https://www.instagram.com/shogi.grenoble",
+        },
+        {
+            id: 7,
+            title: "Epitanime section Shogi",
+            adress: [["24 rue Pasteur","94270 Le Kremlin-Bicêtre"]],
+            horaire: [
+                "Séances hebdomadaires le lundi de 19:00 à 22:30"
+            ], 
+            contact: ["jtj@epitanime.com", "+33 6 11 77 62 99"],
+            website: "ttps://www.epitanime.com/section-jeux-traditionnels-japonais/",
+        },
+        {
+            id: 8,
+            title: "Renmei Anjou Shogi",
+            adress: [["34 rue de la Chambre aux Deniers","49000 Angers"]],
+            horaire: [
+                "Séances hebdomadaires le Jeudi soir de 19:00 à 21:00"
+            ], 
+            contact: ["cyrilmeyzonnier@icloud.com"],
+            website: "http://ras.angers.shogi.free.fr",
+        },
+        {
+            id: 9,
+            title: "Shogi Dojo Lyon",
+            adress: [["5 Place du Marché","69009 Lyon"]],
+            horaire: [
+                "Séances hebdomadaires tous les vendredi de 19h à 22h au Lyon Olympique Echecs"
+            ], 
+            contact: ["shogidojolyon@gmail.com", "04 78 28 85 87"],
+            website: "https://sites.google.com/lyon-olympique-echecs.com/shogi-dojo-lyon/pr%C3%A9sentation",
+        },
+    ]
